b3dd41aec87f547108963d74d684a58a9f880c08
viswaraavi <viswaraavi@yahoo.in>
2016-09-25 17:34:51 -0400

    bandwidth memory calculated

---
+++ bandwidth.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include "lib/rdtsc.h"
+
+#define SIZE (1*1024*1024*1024LL)
+
+size_t array[SIZE];
+
+unsigned long long write_memory(long long size) {
+  unsigned long long start;
+  unsigned long long end;
+  unsigned int check=0;
+  start=rdtsc();
+  for (int i = 0; i < size / sizeof(size_t); i++) {
+  array[i]=1;
+  check++;
+  }
+  end=rdtsc();
+  assert(!(check==0));
+  unsigned long long diff;
+  diff=end-start;
+  return diff;
+}
+
+unsigned long long read_memory(long long size) {
+  unsigned long long start;
+  unsigned long long end;
+  unsigned int dum=0;
+  start=rdtsc();
+  for (int i = 0; i < size / sizeof(size_t); i++) {
+
+   dum+=array[i];
+  
+  }
+  end=rdtsc();
+  assert(!(dum==0));
+  unsigned long long diff;
+  diff=end-start;
+  return diff;
+}
+
+int main()
+{
+memset(array,0xFF,SIZE);
+unsigned long long readbw=read_memory(SIZE);
+unsigned long long writebw=write_memory(SIZE);
+printf("latency for ready time %llu\n latency for write time %llu\n memory bandwidth while reading %f\n memory bandwidth while writing %f \n",readbw,writebw,1024/((double)readbw/3e9),1024/((double)writebw/3e9));
+}
+
+
+
+
